a19d28958857c538a020f1032e326cf32f2a9200
sumayya <sumayyasafoora0@gmail.com>
2025-01-22 15:50:21 +0100

    W15-D03-Gradle Done

---
+++ labs/01-Gradle-Bank-Application/src/main/java/CheckingAccount.java
@@ -0,0 +1,26 @@
+public class CheckingAccount extends Account {
+    public static int OVERDRAFT_LIMIT = 2; //static variables
+    private int overDraftCount;
+    public static int PENALTY = 50;
+
+    public CheckingAccount(String accountHolder, double balance, double withdrawLimit) {
+        super(accountHolder, balance, withdrawLimit);
+        this.overDraftCount = 0;
+    }
+
+    public int getOverDraftCount() {
+        return overDraftCount;
+    }
+
+    @Override
+    public boolean isOverdraftsAllowed() {
+
+        return true;
+    }
+    public void incrementOverDraftCount(){
+        this.overDraftCount++;
+    }
+    public void resetOverDraftCount(){
+        this.overDraftCount = 0;
+    }
+}
